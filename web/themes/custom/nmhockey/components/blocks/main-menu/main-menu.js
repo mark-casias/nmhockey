5196a064472ea0981440e554f828bd6c6ba87a2b
Mark Casias <mark.casias@lullabot.com>
2024-12-27 19:10:40 -0700

    hamburgurler..

--- web/themes/custom/nmhockey/components/blocks/main-menu/main-menu.js
+++ web/themes/custom/nmhockey/components/blocks/main-menu/main-menu.js
@@ -3,12 +3,14 @@
     const menu = document.getElementById('main-nav');
     if (menu) {
       const expandMenu = menu.getElementsByClassName('expand-sub');
+      const hamburger = document.querySelector('.hamburger');
 
       // Mobile Menu Show/Hide.
       toggleExpand.addEventListener('click', (e) => {
         console.log(toggleExpand.classList);
         toggleExpand.classList.toggle('toggle-expand--open');
         menu.classList.toggle('main-nav--open');
+        hamburger.classList.toggle('is-active');
         e.preventDefault();
       });
 
